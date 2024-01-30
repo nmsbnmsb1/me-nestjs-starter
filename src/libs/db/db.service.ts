894e20ff67663ae8a8cdb12fc325b34dc67caa13
nmsbnmsb1 <nmsbnmsb1@126.com>
2024-01-30 16:45:43 +0700

    fix db.service

--- src/libs/db/db.service.ts
+++ src/libs/db/db.service.ts
@@ -6,10 +6,18 @@ import { Sequelize, ModelCtor, Model, getAttributes } from 'sequelize-typescript
 import * as CDB from 'me-cache-db';
 
 export type DB = string | Sequelize;
-export type Repo = ModelCtor;
 export type RepoOptions = { db: DB; tbn: string; tmodel: ModelCtor };
 export type RepoOptionsTbnLike = { db: DB; tbn?: string; tbnLike?: string; tmodel: ModelCtor };
-export type RepoData = { db: string; tbn: string; tmodel: Repo; repo: Repo; rid: string };
+//
+export type Repo = ModelCtor;
+export type RepoData = {
+	db: Sequelize;
+	dbn: string;
+	tbn: string;
+	tmodel: ModelCtor;
+	repo: Repo;
+	rid: string;
+};
 
 @Injectable()
 export class DBService {
@@ -26,16 +34,20 @@ export class DBService {
 		let sequelize = this.getDBConnection(db);
 		if (!sequelize.isDefined(tbn)) {
 			await sequelize
-				.define(tbn, getAttributes(tmodel.prototype || tmodel), {
-					tableName: tbn,
-					timestamps: true,
-					paranoid: true,
-				})
+				.define(tbn, getAttributes(tmodel.prototype || tmodel), { tableName: tbn, timestamps: true, paranoid: true })
 				.sync();
 		}
 		//
 		let repo: any = sequelize.models[tbn];
-		if (!repo.$options) repo.$options = { rid: `${db}.${tbn}`, db, tbn, tmodel, repo };
+		if (!repo.$options)
+			repo.$options = {
+				db: sequelize,
+				dbn: sequelize.getDatabaseName(),
+				tbn,
+				tmodel,
+				repo,
+				rid: `${sequelize.getDatabaseName()}.${tbn}`,
+			};
 		return repo as Repo;
 	}
 	public getRepoData(r: Repo | Model | RepoOptions | any): any {
@@ -43,32 +55,34 @@ export class DBService {
 		//
 		if (r instanceof Model) {
 			let options: any = {};
-			options.db = r.sequelize.getDatabaseName();
+			options.db = r.sequelize;
+			options.dbn = r.sequelize.getDatabaseName();
 			options.tbn = (r.constructor as any).getTableName();
 			options.tmodel = r.constructor;
 			options.repo = r.constructor;
-			options.rid = `${options.db}.${options.tbn}`;
+			options.rid = `${options.dbn}.${options.tbn}`;
 			return ((r as any).$options = options);
 		}
 		if (typeof r === 'function') {
 			let options: any = {};
-			options.db = r.sequelize.getDatabaseName();
+			options.db = r.sequelize;
+			options.dbn = r.sequelize.getDatabaseName();
 			options.tbn = r.getTableName();
 			options.tmodel = r;
 			options.repo = r;
-			options.rid = `${options.db}.${options.tbn}`;
+			options.rid = `${options.dbn}.${options.tbn}`;
 			return (r.$options = options);
 		}
 
 		//
-		let db = typeof r.db === 'string' ? r.db : r.db.getDatabaseName();
+		let sequelize = this.getDBConnection(r.db);
+		let dbn = sequelize.getDatabaseName();
 		let tbn = r.tbn;
-		let sequelize = this.getDBConnection(db);
 		if (sequelize.isDefined(tbn)) {
-			let options: any = { db, tbn };
+			let options: any = { db: sequelize, dbn, tbn };
 			options.tmodel = r.tmodel;
 			options.repo = sequelize.models[options.tbn];
-			options.rid = `${options.db}.${options.tbn}`;
+			options.rid = `${options.dbn}.${options.tbn}`;
 			return ((r as any).$options = options);
 		}
 		//
